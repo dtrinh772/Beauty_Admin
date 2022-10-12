64f9a24e21446adeaee01b1a583e43215a9763b9
dtrinh772@gmail.com <dtrinh772@gmail.com>
2022-10-12 13:22:26 +0100

    router route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Layout from '../layout/Layout.vue'
+import Layout from "../layout/Layout.vue";
+import Appointment from "../views/HomeView.vue";
+import Websetting from "../views/AboutView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Layout,
+      children: [
+        {
+          path: "appointment",
+          component: Appointment,
+        },
+        {
+          path: "setting",
+          component: Websetting,
+        },
+      ],
     },
   ],
 });
